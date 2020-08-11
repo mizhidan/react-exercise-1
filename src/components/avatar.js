b522ddf3da127197405b1598625f8df72eaf74d0
mizhidan <dj9824@126.com>
2020-08-11 19:27:31 +0800

    [danjie] feat: avatar icon

---
+++ src/components/avatar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './avatar.css';
+
+const Avatar = () => (
+  <div className="avatardiv">
+    <img
+      // eslint-disable-next-line no-undef
+      src={require('../assets/avatar.jpg')}
+      alt="avatar"
+      className="avatar"
+    ></img>
+  </div>
+);
+
+export default Avatar;
