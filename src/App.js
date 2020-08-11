760a8e2f44a9d06062eacc313841b6552b8f48f5
mizhidan <dj9824@126.com>
2020-08-11 19:56:40 +0800

    [danjie] feat: self introduce

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.scss';
 import Avatar from './components/avatar';
+import Introduce from './components/introduce';
 
 class App extends Component {
   render() {
     return (
       <main className="app">
         <Avatar />
+        <Introduce />
       </main>
     );
   }
---
+++ src/components/introduce.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './introduce.css';
+
+const Introdce = () => (
+  <div className="introduce">
+    <h1>Hello,</h1>
+    <h2>MY NAME IS KAMIL 24YO AND THIS IS MY RESUME/CV</h2>
+  </div>
+);
+
+export default Introdce;
